de3300e530e00183aa6a6063fc5581019109a2f2
Issam E. Maghni <issam.e.maghni@mailbox.org>
2021-03-07 18:12:29 -0500

    launch: Split command into two

--- launch.sh
+++ launch.sh
@@ -1,1 +1,11 @@
-CRAZYRADIO=$(lsusb -d 1915:7777 | awk -F '[ :]' '{printf "/dev/bus/usb/%s/%s\n", $2, $4}') docker-compose up --build
+#!/bin/sh -e
+
+export CRAZYRADIO=$(lsusb -d 1915:7777 | awk -F '[ :]' '{printf "/dev/bus/usb/%s/%s\n", $2, $4}')
+
+echo
+echo '============================= BUILDING THE IMAGES =============================='
+docker-compose build
+
+echo
+echo '============================ RUNNING THE CONTAINERS ============================'
+exec docker-compose up
