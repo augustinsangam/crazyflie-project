e7548574d8a6572e3669183246c0053868dda53b
Eya-Tom Augustin SANGAM <eya-tom-augustin.sangam@polymtl.ca>
2021-04-20 21:23:26 -0400

    Update submodules

--- launch.sh
+++ launch.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -e
 
+clear
+
+export "LAUNCH_ARG=${1}"
+
 export CRAZYRADIO=$(lsusb -d 1915:7777 | awk -F '[ :]' '{printf "/dev/bus/usb/%s/%s\n", $2, $4}')
+
+test -d out || mkdir out
 
 echo
 echo '============================= BUILDING THE IMAGES =============================='
